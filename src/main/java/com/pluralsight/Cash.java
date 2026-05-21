3f48214368ffb083cbd3ffe963e7b96209359db1
ezrateshale <ezra.teshale12@gmail.com>
2026-05-21 11:47:54 -0500

    Test commit

--- src/main/java/com/pluralsight/Cash.java
+++ src/main/java/com/pluralsight/Cash.java
@@ -1,12 +1,13 @@
 package com.pluralsight;
 
-public class Cash extends Asset{
+public class Cash extends Asset {
 
-    public Cash(double originalCost, String description, String dateAcquired){
+    public Cash(double originalCost, String description, String dateAcquired) {
         super(description, dateAcquired, originalCost);
     }
-  @Override
-  public double getValue() {
-      return getOriginalCost();
-  }
+
+    @Override
+    public double getValue() {
+        return getOriginalCost();
+    }
 }
